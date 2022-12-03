7e44dca4e01a24a48e774d0b58d36ed0a5134f63
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-03 11:14:44 +0000

    Day 1 pass

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -5,7 +5,11 @@ fn main() {
 
     let fs = std::fs::read_to_string("./src/bin/day1_input.txt").unwrap_or_else(|e| panic!("{e}"));
 
+<<<<<<< HEAD
     let mut out = fs.split("\n\n")
+=======
+    let out = fs.split("\n\n")
+>>>>>>> 62d620e (Day 1 pass)
         .into_iter()
         .map(|e| e.split('\n'))
         .map(|v|
@@ -15,7 +19,12 @@ fn main() {
             out.push(v.iter().sum::<u64>());
             out
         });
+<<<<<<< HEAD
     println!("{:?}",out.iter().take(3).collect::<Vec<_>>());
     println!("{:?}",out.iter().take(3).sum::<u64>());
+=======
+    println!("Q1: {:?}",out.iter().take(3).collect::<Vec<_>>());
+    println!("Q2: {:?}",out.iter().take(3).sum::<u64>());
+>>>>>>> 62d620e (Day 1 pass)
 
 }
