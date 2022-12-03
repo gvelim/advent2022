0dca1251b74cc0f4a5acd02e1c44ba3d886b0fc9
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-03 19:07:00 +0000

    Day 3 - part 1 pass

---
+++ src/bin/day3.rs
@@ -0,0 +1,35 @@
+extern crate core;
+
+use std::collections::HashSet;
+
+fn main() {
+    // let test = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+    // jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
+    // PmmdzqPrVvPwwTWBwg\n\
+    // wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
+    // ttgJtRGJQctTZtZT\n\
+    // CrZsJsPPZsGzwwsLwLmpwMDw";
+    let file = std::fs::read_to_string("./src/bin/day3.txt").unwrap_or_else(|e| panic!("{e}"));
+
+    let out = file.lines()
+        .map(|line| line.split_at( line.len()>>1 ) )
+        .map(|(compa, compb)| {
+            println!("{compa},{compb}");
+            compa.chars().filter(|&c| compb.find(c).is_some() ).collect::<HashSet<_>>()
+        })
+        .map(|set|
+            set.into_iter()
+                .map(|c|
+                    match c {
+                        'a'..='z' => u32::from(c) - u32::from('a') + 1,
+                        'A'..='Z' => u32::from(c) - u32::from('A') + 27,
+                        _ => panic!("use only alphabetic characters")
+                    }
+                )
+                .collect::<Vec<u32>>()
+        )
+        .fold(0,|sum, v|
+            sum + v.iter().sum::<u32>()
+        );
+    println!("{:?}",out);
+}
