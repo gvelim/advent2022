5d9a62d8ae72e765418b299611edf1c70032d41b
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-05 17:47:02 +0000

    Day 3 - pass

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
 fn main() {
-    // let test = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
+    // let lines = "vJrwpWtwJgWrhcsFMMfFFhFp\n\
     // jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n\
     // PmmdzqPrVvPwwTWBwg\n\
     // wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
@@ -11,13 +11,42 @@ fn main() {
     let lines = std::fs::read_to_string("./src/bin/day3.txt").unwrap_or_else(|e| panic!("{e}"));
 
     println!("{:?}",component_1(&lines));
+    println!("{:?}",component_2(&lines));
+}
+
+fn component_2(lines:&str) -> u32 {
+    lines.lines()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|group| {
+            group.into_iter()
+                .map(|a| a.chars().collect::<HashSet<_>>())
+                .reduce(|a, b|
+                    a.intersection(&b).copied().collect::<HashSet<_>>()
+                )
+                .unwrap_or_else(|| panic!("Ops!"))
+        })
+        .map(|set|
+            set.into_iter()
+                .map(|c|
+                    match c {
+                        'a'..='z' => u32::from(c) - u32::from('a') + 1,
+                        'A'..='Z' => u32::from(c) - u32::from('A') + 27,
+                        _ => panic!("use only alphabetic characters")
+                    }
+                )
+                .sum::<u32>()
+        )
+        .sum::<u32>()
 }
 
 fn component_1(lines: &str) -> u32 {
     lines.lines()
         .map(|line| line.split_at( line.len()>>1 ) )
         .map(|(compa, compb)| {
-            compa.chars().filter(|&c| compb.find(c).is_some() ).collect::<HashSet<_>>()
+            compa.chars()
+                .filter(|&c| compb.find(c).is_some() )
+                .collect::<HashSet<_>>()
         })
         .map(|set|
             set.into_iter()
