b4c8a8827078e74eacabaead2b8a075e314b2484
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-06 08:19:58 +0000

    Day 4 - minor refactor

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 fn main() {
 
     let data = std::fs::read_to_string("src/bin/day4_input.txt").expect("Ops! Cannot read file");
-    let iterator = data.lines()
+    let pairs = data.lines()
         .map(|line|
             line.split(|c:char| c.is_ascii_punctuation())
                 .map(|c| u32::from_str(c).unwrap_or_else(|e| panic!("{e}")) )
@@ -18,13 +18,18 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let out = iterator.iter()
-        .filter(|(a,b)| a.is_subset(&b) || b.is_subset(&a))
+    let out = pairs.iter()
+        .filter(|(a,b)|
+            match  a.len() < b.len() {
+                true => a.is_subset(&b),
+                false => b.is_subset(&a)
+            }
+        )
         .count();
     println!("Component 1 = {out}");
 
-    let len = iterator.len();
-    let out = len - iterator.iter()
+    let len = pairs.len();
+    let out = len - pairs.iter()
         .filter(|(a,b)| a.is_disjoint(&b) )
         .count();
     println!("Component 2 = {out}");
