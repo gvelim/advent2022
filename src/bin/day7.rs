2074e1e76306e74502f86e177d812113bf0b7a5e
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-10 17:38:58 +0000

    Day 7 - Pass 1 - golden star

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -29,8 +29,8 @@ struct Tree {
     totals: RefCell<Vec<(String,usize)>>
 }
 impl Tree {
-    fn children(&self, dir: &String) -> Vec<&String> {
-        self.map[dir]
+    fn children(&self, path: &String) -> Vec<&String> {
+        self.map[path]
             .content
             .iter()
             .filter_map(|rt|
@@ -42,8 +42,8 @@ impl Tree {
             )
             .collect()
     }
-    fn dir_size(&self, dir: &String) -> usize {
-        self.map[dir].size
+    fn dir_size(&self, path: &String) -> usize {
+        self.map[path].size
     }
     fn totals(&self) -> Vec<(String,usize)> {
         self.totals.take()
@@ -52,22 +52,22 @@ impl Tree {
         use LineType::*;
 
         let mut map = HashMap::<String,Node>::new();
-        let mut dir: String = "".to_string();
+        let mut path: String = "".to_string();
 
         history.iter()
             // .inspect(|line| println!("{:?}",line))
             .for_each(|lt| {
                 match lt {
-                    Cmd(CommandType::Cd(d)) if d.contains("..") => dir = map[&dir].parent.clone(),
-                    Cmd(CommandType::Cd(d)) => {
-                        let id = format!("{}{}",dir,d);
-                        println!("{id}");
-                        map.entry(id.clone())
-                            .or_insert(Node { parent: dir.clone(), content: Vec::new(), size: 0 });
-                        dir = id;
+                    Cmd(CommandType::Cd(dir)) if dir.contains("..") => path = map[&path].parent.clone(),
+                    Cmd(CommandType::Cd(dir)) => {
+                        let cpath = format!("{}{}", path, dir);
+                        // println!("{cpath}");
+                        map.entry(cpath.clone())
+                            .or_insert(Node { parent: path.clone(), content: Vec::new(), size: 0 });
+                        path = cpath;
                     }
                     Rst(res) => {
-                        let node = map.get_mut(&dir).unwrap();
+                        let node = map.get_mut(&path).unwrap();
                         node.content.push(res.clone());
                         if let &ResultType::File(_,fsize) = res {
                             node.size += fsize;
@@ -78,64 +78,55 @@ impl Tree {
             });
         Tree { map, totals: RefCell::new(Vec::new()) }
     }
-    fn calc_dirs_totals(&self, dir: &String) -> usize {
-        let mut sum = self.dir_size(dir);
-        for d in self.children(dir) {
-            let key = format!("{}{}",dir,d);
-            sum += self.calc_dirs_totals(&key);
+    fn calc_dirs_totals(&self, path: &String) -> usize {
+        let mut sum = self.dir_size(path);
+        for dir in self.children(path) {
+            let cpath = format!("{}{}", path, dir);
+            sum += self.calc_dirs_totals(&cpath);
         }
-        println!("{:?}:{:?}",dir, sum);
-        self.totals.borrow_mut().push((dir.clone(),sum));
+        // println!("{:?}:{:?}", path, sum);
+        self.totals.borrow_mut().push((path.clone(), sum));
         sum
     }
 }
 
+struct History();
+impl History {
+    fn parse(history:&str) -> Vec<LineType> {
+        history.lines()
+            .filter_map(|e| {
+                let p:Vec<_> = e.split(" ").collect();
+                match p[0] {
+                    "$" => match p[1] {
+                        "ls" => Some(LineType::Cmd(CommandType::List)),
+                        "cd" => Some(LineType::Cmd(CommandType::Cd(String::from(p[2])))),
+                        _ => None
+                    }
+                    "dir" => Some(LineType::Rst(ResultType::Dir(p[1].to_string()))),
+                    _ => Some(LineType::Rst(ResultType::File(p[1].to_string(), usize::from_str(p[0]).unwrap())))
+                }
+            })
+            // .inspect(|e| println!("{:?}",e))
+            .collect::<Vec<_>>()
+    }
+}
 
 fn main() {
-    let history = "$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k";
 
-    // let history = std::fs::read_to_string("src/bin/day7_input.txt").expect("");
+    let history = std::fs::read_to_string("src/bin/day7_input.txt").expect("");
 
-    let out = history.lines()
-        .filter_map(|e| {
-            let p:Vec<_> = e.split(" ").collect();
-            match p[0] {
-                "$" => match p[1] {
-                    "ls" => Some(LineType::Cmd(CommandType::List)),
-                    "cd" => Some(LineType::Cmd(CommandType::Cd(String::from(p[2])))),
-                    _ => None
-                }
-                "dir" => Some(LineType::Rst(ResultType::Dir(p[1].to_string()))),
-                _ => Some(LineType::Rst(ResultType::File(p[1].to_string(), usize::from_str(p[0]).unwrap())))
-                }
-        })
-        .inspect(|e| println!("{:?}",e))
-        .collect::<Vec<_>>();
+    let hst_lines = History::parse(history.as_str());
 
-    let tree = Tree::parse_history(&out);
-    println!("{:?}",tree);
+    let tree = Tree::parse_history(&hst_lines);
+
     tree.calc_dirs_totals(&"/".to_string());
-    println!("{:?}", tree.totals());
+
+    println!("Directories < 100000 \n====================");
+    println!("{:?}",
+             tree.totals()
+                 .iter()
+                 .filter(|(_,size)| *size < 100000 )
+                 .inspect(|&p| println!("{:?}",p))
+                 .map(|&(_,size)| size)
+                 .sum::<usize>());
 }
