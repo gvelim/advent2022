f2bdbb09eeda57dfc48137c0a07153ef01d3fe91
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-20 22:55:56 +0000

    Day 12 - WIP - extract start, finish points - calibrate elevations to start from 0-25 incl. S & E points

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -6,21 +6,20 @@ use std::fmt::{Debug, Formatter};
 
 fn main() {
     let input = "Sabqponm\nabcryxxl\naccszExk\nacctuvwj\nabdefghi";
+    // let input = std::fs::read_to_string("src/bin/day12_input.txt").expect("ops!");
 
-    let mut grid = parse_forest(input);
+    let (grid,start,finish) = parse_forest(input);
     let mut visited: Grid<(bool,Option<Coord>)> = Grid::new(grid.width, grid.height);
     let mut queue = BinaryHeap::<Step>::new();
     println!("{:?}\n{:?}",grid,visited);
 
     // push start in the queue
-    *grid.cell_mut((0,0).into()).unwrap() = 96;
-    *grid.cell_mut((5,2).into()).unwrap() = 123;
-    queue.push(Step(*grid.cell((0,0).into()).unwrap(), Coord::from((0,0))) );
+    queue.push(Step(*grid.cell(start).unwrap(), start) );
     let mut path =  Vec::<_>::new();
     // pop from top & while still nodes in the queue
     while let Some(Step(_,p)) = queue.pop() {
         println!("Popped: {:?}",p);
-        if p.eq( &(5,2).into()) {
+        if p.eq( &finish) {
             // found target node
             let mut cur = p;
             while let Some(par) = visited.cell(cur).unwrap().1 {
@@ -30,9 +29,6 @@ fn main() {
             break
         }
         // mark position as visited
-        if visited.cell_mut(p).unwrap().0 {
-            continue
-        }
         visited.cell_mut(p).unwrap().0 = true;
         let &node = grid.cell(p).unwrap();
 
@@ -64,41 +60,48 @@ fn main() {
 
 }
 
-fn parse_forest(data: &str) -> Grid<u8> {
+fn parse_forest(data: &str) -> (Grid<u8>,Coord,Coord) {
     let width = data.lines().next().unwrap().len();
     let height = data.lines().count();
     let mut grid = Grid::new(width,height);
+    let (mut start, mut finish) = ((0,0).into(),(0,0).into());
 
     for (y,line) in data.lines().enumerate() {
         for (x, val) in line.bytes().enumerate() {
-            *grid.cell_mut((x,y).into()).unwrap() = val;
+            match val {
+                b'S' => {
+                    start = (x, y).into();
+                    *grid.cell_mut((x,y).into()).unwrap() = 0;
+                },
+                b'E' => {
+                    finish = (x, y).into();
+                    *grid.cell_mut((x, y).into()).unwrap() = b'z'-b'a'+2;
+                }
+                _ => *grid.cell_mut((x,y).into()).unwrap() = val - b'a' + 1
+            }
         }
     }
-    grid
+    (grid, start, finish)
 }
-struct Step(u8, Coord);
 
+struct Step(u8, Coord);
 impl PartialEq<Self> for Step {
     fn eq(&self, other: &Self) -> bool {
         self.0.eq(&other.0)
     }
 }
-
-
 impl Eq for Step {}
-
-
 impl PartialOrd<Self> for Step {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.0.cmp(&other.0))
     }
 }
-
 impl Ord for Step {
     fn cmp(&self, other: &Self) -> Ordering {
         other.0.cmp(&self.0)
     }
 }
+
 #[derive(Copy, Clone, Eq, PartialEq)]
 struct Coord {
     x: usize,
@@ -153,7 +156,7 @@ impl<T> Debug for Grid<T>
         (0..self.height).for_each(|y|{
             (0..self.width).for_each(|x| {
                 let cell = self.cell((x,y).into()).unwrap();
-                write!(f, "{:03?}|",cell).expect("failed in x");
+                write!(f, "{:4?}|",cell).expect("failed in x");
             });
             writeln!(f).expect("failed in y");
         });
