fc309e61b3a0eff30a3cfd3470f81798c88bb1b1
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-22 23:29:51 +0000

    Day 13 - WIP - Parsing the input

---
+++ src/bin/day13.rs
@@ -0,0 +1,109 @@
+use std::fmt::{Debug, Formatter};
+use std::iter::Peekable;
+use std::str::FromStr;
+use crate::ListItem::{L, N};
+
+fn main() {
+let input = "[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]";
+
+    input.split("\n\n")
+        .into_iter()
+        .map(|x| x.lines().collect::<Vec<_>>() )
+        .map(|d|
+                 (ListItem::parse(d[0]), ListItem::parse(d[1]))
+        )
+        .inspect(|l| println!("{:?}",l))
+        .all(|_| true);
+}
+
+
+enum ListItem {
+    N(u8),
+    L(Vec<ListItem>)
+}
+impl ListItem {
+    fn parse(inp: &str) -> ListItem {
+
+        struct Scanner<I: Iterator<Item=char>> {
+            i: Peekable<I>,
+        }
+        impl<I: Iterator<Item=char>> Scanner<I> {
+            fn new(s: I) -> Self {
+                Scanner { i: s.peekable() }
+            }
+            fn parse_list(&mut self) -> ListItem {
+                let mut s = String::new();
+                let mut v = L(vec![]);
+                loop {
+                    // println!("{:?}, {:?}, {:?}",v, s, self.i.peek());
+                    match &self.i.peek() {
+                        Some('[') => {
+                            self.i.next();
+                            v.insert( self.parse_list());
+                        },
+                        Some(&c@ '0'..='9') => s.push(c),
+                        &c@
+                        (Some(',') | Some(']')) if !s.is_empty() => {
+                            v.insert(N(u8::from_str(s.as_str()).expect("")));
+                            s.clear();
+                            if ']'.eq(c.unwrap()) {
+                                break v
+                            }
+                        },
+                        Some(',') | Some(']') => {}
+                        None => break v,
+                        _ => unreachable!()
+                    }
+                    self.i.next();
+                }
+            }
+        }
+        Scanner::new(inp.chars().peekable()).parse_list()
+    }
+    fn insert(&mut self, item:ListItem) {
+        match (self,item) {
+            (L(list), item) => list.push(item),
+            (N(old), N(new)) => *old = new,
+            (_,_) => unreachable!()
+        }
+    }
+}
+impl Debug for ListItem {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            N(n) => write!(f,"{n}")?,
+            L(v) => {
+                write!(f,"[")?;
+                let len = v.len();
+                for (i,li) in v.iter().enumerate() {
+                    li.fmt(f)?;
+                    if i < len-1 { write!(f,",")?; }
+                }
+                write!(f,"]")?;
+            }
+        };
+        Ok(())
+    }
+}
