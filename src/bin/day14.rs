37e87bf7b0d7550f420a5cd285ae03c9a98be682
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-28 15:03:22 +0000

    Day 14 - Refactoring - parsing - cosmetics

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,5 +1,5 @@
-use std::cell::RefCell;
 use std::fmt::{Debug, Formatter};
+use std::num::ParseIntError;
 use std::str::FromStr;
 
 fn main() {
@@ -13,7 +13,7 @@ fn main() {
     let mut board: Board<Mat> = Board::new((max.x<<1)+1,max.y+2+1);
 
     // paint layout on the board
-    let painter = board.get_painter();
+    let mut painter = board.new_painter();
     plines
         .into_iter()
         .for_each(|pline|
@@ -27,9 +27,9 @@ fn main() {
     );
     println!("Scenario 1: Grains Rest: {}\n{:?}", board.grains_rest()-1, board);
 
-    board.empty_sand();
+    board.reset();
     // add rock floor
-    board.get_painter().rock_wall((0,max.y+2).into(), (max.x<<1,max.y+2).into());
+    board.new_painter().rock_wall((0, max.y+2).into(), (max.x<<1, max.y+2).into());
     // run the sand simulation until grain settled position == starting position
     board.run(
         start, |g| g.pos.eq(&start)
@@ -43,16 +43,8 @@ fn parse_plines(input:&str) -> (Coord, Vec<Vec<Coord>>) {
     let plines =
         input.lines()
             .map(|line|{
-                line.split(&[',','-','>'])
-                    .map(|p| p.trim())
-                    .filter(|p| !p.is_empty() )
-                    .map(|val| usize::from_str(val).expect("ops!"))
-                    .collect::<Vec<_>>()
-            })
-            .map(|pline| {
-                pline
-                    .chunks(2)
-                    .map(|p| Coord{ x:p[0], y:p[1] })
+                line.split(" -> ")
+                    .map(|val| Coord::from_str(val).expect("Ops!"))
                     .inspect(|p|{
                         if max.x < p.x { max.x = p.x }
                         if max.y < p.y { max.y = p.y }
@@ -69,32 +61,131 @@ fn parse_plines(input:&str) -> (Coord, Vec<Vec<Coord>>) {
 struct Grain<'a> {
     pos: Coord,
     settled: bool,
-    board: RefCell<&'a mut Board<Mat>>
+    board: &'a Board<Mat>
 }
 
 impl Grain<'_> {
-    fn move_one(&mut self) -> Option<Coord> {
+    fn fall(&mut self) -> Option<Coord> {
+
         if self.settled { return None }
-        let board = self.board.borrow_mut();
+
         let Coord{ x, y} = self.pos;
 
-        let l = board.square( (x-1,y+1).into() );
-        let u = board.square( (x,y+1).into() );
-        let r = board.square( (x+1,y+1).into() );
+        let [lc, uc, rc] = [(x-1, y+1).into(), (x, y+1).into(), (x+1, y+1).into()];
+
+        let l = self.board.square( lc );
+        let u = self.board.square( uc );
+        let r = self.board.square( rc );
 
         match (l,u,r) {
-            (_, Some(Mat::Air), _) => self.pos = (x,y+1).into(),
-            (Some(Mat::Air), _, _) => self.pos = (x-1,y+1).into(),
-            (_, _, Some(Mat::Air)) => self.pos = (x+1,y+1).into(),
-            (_, None, _) => return None,
-            (_, _, _) => self.settled = true
+            (_, None, _) => None,
+            (_, Some(Mat::Air), _) => { self.pos = uc; Some(self.pos) },
+            (Some(Mat::Air), _, _) => { self.pos = lc; Some(self.pos) },
+            (_, _, Some(Mat::Air)) => { self.pos = rc; Some(self.pos) },
+            (_, _, _) => { self.settled = true; None }
         }
-        Some(self.pos)
     }
     fn is_settled(&self) -> bool {
         self.settled
     }
 }
+
+
+#[derive(PartialEq, Copy, Clone)]
+enum Mat { Rock, Sand, Air }
+impl Default for Mat {
+    fn default() -> Self { Mat::Air }
+}
+
+impl Board<Mat> {
+    fn new_painter(&mut self) -> Painter {
+        Painter { board: self }
+    }
+    fn release_grain(&mut self, pos: Coord) -> Grain {
+        Grain { pos, settled: false, board: self }
+    }
+    fn grains_rest(&self) -> usize {
+        self.grid.iter()
+            .filter(|&s| *s == Mat::Sand )
+            .count()
+    }
+    fn reset(&mut self) -> usize {
+        self.grid.iter_mut()
+            .filter(|s| **s == Mat::Sand )
+            .map(|s| *s = Mat::Air)
+            .count()
+    }
+    fn run<F>(&mut self, start: Coord, check_goal: F) where F: Fn(&Grain) -> bool {
+
+        loop {
+            let mut grain = self.release_grain(start);
+
+            // let the grain fall until it either (a) settles or (b) falls off the board
+            while let Some(_) = grain.fall() {};
+
+            let pos = grain.pos;
+            // Have we reached an end state ?
+                // we use a closure that passes the stopped grain
+                // for checking whether (a) it has fallen in the abyss or (b) reached the starting position
+            if check_goal(&grain) {
+                // Mark settled grain position on the board
+                *self.square_mut(pos).unwrap() = Mat::Sand;
+                break
+            }
+
+            // Mark settled grain position on the board
+            *self.square_mut(pos).unwrap() = Mat::Sand;
+        }
+    }
+}
+
+struct Painter<'a> {
+    board: &'a mut Board<Mat>
+}
+
+impl Painter<'_> {
+    fn rock_wall(&mut self, a: Coord, b: Coord) {
+        let x_range = if a.x <= b.x { a.x ..= b.x } else { b.x ..= a.x };
+        x_range
+            .flat_map(|x| {
+                let y_range = if a.y <= b.y { a.y..=b.y } else { b.y..=a.y };
+                y_range.map(move |y| (x, y).into())
+            })
+            .for_each(|p|
+                *self.board.square_mut(p).unwrap() = Mat::Rock
+            );
+    }
+    fn rock_walls(&mut self, c: &[Coord]) {
+        c.windows(2)
+            .for_each(| p|
+                self.rock_wall(p[0], p[1])
+            );
+    }
+}
+
+impl Debug for Board<Mat> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f,"   |").expect("failed in y");
+        (0..self.width).for_each(|x| { write!(f, "{:^3}",x).expect("ops") });
+        writeln!(f).expect("");
+        (0..self.height).for_each(|y|{
+            write!(f,"{y:3}|").expect("failed in y");
+            (0..self.width).for_each(|x| {
+                write!(f, "{:^3}",
+                       match self.square((x, y).into()).unwrap() {
+                           Mat::Air => '.',
+                           Mat::Rock => '#',
+                           Mat::Sand => 'o',
+                       }).expect("Ops!")
+            });
+            writeln!(f).expect("failed in y");
+        });
+        Ok(())
+    }
+}
+
+/// Generics
+///
 
 #[derive(Ord, PartialOrd,Copy, Clone, Eq, PartialEq)]
 struct Coord {
@@ -112,78 +203,16 @@ impl From<(usize,usize)> for Coord {
     }
 }
 
-#[derive(PartialEq, Copy, Clone)]
-enum Mat { Rock, Sand, Air }
-impl Default for Mat {
-    fn default() -> Self { Mat::Air }
-}
+impl FromStr for Coord {
+    type Err = ParseIntError;
 
-impl Board<Mat> {
-    fn get_painter(&mut self) -> Painter {
-        Painter { board: RefCell::new(self) }
-    }
-    fn get_grain(&mut self, pos: Coord) -> Grain {
-        Grain { pos, settled: false, board: RefCell::new(self) }
-    }
-    fn grains_rest(&self) -> usize {
-        self.grid.iter()
-            .filter(|&s| *s == Mat::Sand )
-            .count()
-    }
-    fn empty_sand(&mut self) -> usize {
-        self.grid.iter_mut()
-            .filter(|s| **s == Mat::Sand )
-            .map(|s| *s = Mat::Air)
-            .count()
-    }
-    fn run<F>(&mut self, start: Coord, goal: F) where F: Fn(&Grain) -> bool {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.trim().split(',').map(|val| usize::from_str(val) );
+        Ok(Coord{
+            x: iter.next().unwrap()?,
+            y: iter.next().unwrap()?,
+        })
 
-        loop {
-            let mut grain = self.get_grain(start);
-
-            // let the grain fall until it either (a) settles or (b) falls off the board
-            while let Some(_) = grain.move_one() {};
-
-            let pos = grain.pos;
-            // we have reached an end state, either
-            if goal(&grain) {
-                *self.square_mut(pos).unwrap() = Mat::Sand;
-                break
-            }
-
-            // Mark settled grain position on the board
-            *self.square_mut(pos).unwrap() = Mat::Sand;
-        }
-    }
-}
-
-struct Painter<'a> {
-    board: RefCell<&'a mut Board<Mat>>
-}
-
-impl Painter<'_> {
-    fn point(&self, p: Coord, mat: Mat) {
-        let mut board = self.board.borrow_mut();
-        *(*board).square_mut(p).unwrap() = mat;
-    }
-    fn rock_wall(&self, a: Coord, b: Coord) {
-        let x_range = if a.x <= b.x { a.x ..= b.x } else { b.x ..= a.x };
-        x_range
-            .flat_map(|x| {
-                let y_range = if a.y <= b.y { a.y..=b.y } else { b.y..=a.y };
-                y_range.map(move |y| (x, y).into())
-            })
-            .all(|p| {
-                self.point(p, Mat::Rock);
-                true
-            });
-    }
-    fn rock_walls(&self, c: &[Coord]) {
-        c.windows(2)
-            .all(| p| {
-                self.rock_wall(p[0], p[1]);
-                true
-            });
     }
 }
 
@@ -215,26 +244,5 @@ impl<T> Board<T>
             return None
         }
         Some(&mut self.grid[p.y * self.width + p.x])
-    }
-}
-
-impl Debug for Board<Mat> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f,"   |").expect("failed in y");
-        (0..self.width).for_each(|x| { write!(f, "{:^3}",x).expect("ops") });
-        writeln!(f).expect("");
-        (0..self.height).for_each(|y|{
-            write!(f,"{y:3}|").expect("failed in y");
-            (0..self.width).for_each(|x| {
-                write!(f, "{:^3}",
-                       match self.square((x, y).into()).unwrap() {
-                           Mat::Air => '.',
-                           Mat::Rock => '#',
-                           Mat::Sand => 'o',
-                }).expect("Ops!")
-            });
-            writeln!(f).expect("failed in y");
-        });
-        Ok(())
     }
 }
