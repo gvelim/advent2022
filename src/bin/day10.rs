8a40670db1392e018784862327ffc08e4c5a5c8b
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-12-29 17:10:53 +0000

    Day 10 - Refactoring - removing RC/RfCell complexity

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,6 +1,5 @@
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::str::FromStr;
+use std::vec::IntoIter;
 
 type Cycles = usize;
 
@@ -29,14 +28,14 @@ struct CPU {
     x: Register,
     buffer: Option<Instruction>,
     exec_cycles: Cycles,
-    ip: Option<Rc<RefCell<dyn Iterator<Item=Instruction>>>>
+    ip: Option<IntoIter<Instruction>>
 }
 impl CPU {
     fn new() -> CPU {
         CPU { x: Register(1), buffer: None, exec_cycles: 0, ip: None }
     }
     fn load(&mut self, ops: Vec<Instruction>) {
-        self.ip = Some(Rc::new(RefCell::new(ops.into_iter())));
+        self.ip = Some(ops.into_iter());
     }
     fn fetch(&mut self, op: Instruction) {
         self.exec_cycles = op.ticks;
@@ -57,8 +56,9 @@ impl CPU {
     }
     fn tick(&mut self) {
         if !self.execute() {
-            let Some(iter) = self.ip.clone() else { panic!("")};
-            self.fetch(iter.borrow_mut().next().unwrap());
+            let mut ip = self.ip.take().unwrap();
+            self.fetch(ip.next().unwrap());
+            self.ip.replace(ip);
         }
     }
     fn reg_x(&self) -> isize {
@@ -89,15 +89,17 @@ impl CRT {
 
 fn parse_instructions(inp: &str) -> (Vec<Instruction>, usize) {
     inp.lines()
-        .map(|line| line.split(' ').collect::<Vec<_>>())
-        .map(|item| {
-            match item[0] {
-                "noop" => Instruction { op: InstructionSet::Noop, ticks: 1 },
-                "addx" => {
-                    let val = isize::from_str(item[1]).unwrap();
+        .map(|line| {
+            let mut iter = line.split(' ');
+            match iter.next() {
+                Some("noop") => Instruction { op: InstructionSet::Noop, ticks: 1 },
+                Some("addx") => {
+                    let val = isize::from_str(
+                        iter.next().expect("parse_instructions: addx is missing its value!")
+                    ).expect("parse_instructions: addx not followed by numeric value!");
                     Instruction { op: InstructionSet::AddX(val), ticks: 2 }
                 },
-                _ => panic!("Woohaa!")
+                _ => panic!("parse_instructions: unknown instruction caught!")
             }
         })
         .fold((vec![],0), |(mut out,mut total), op| {
@@ -110,15 +112,14 @@ fn parse_instructions(inp: &str) -> (Vec<Instruction>, usize) {
 fn main() {
     let input = std::fs::read_to_string("src/bin/day10_input.txt").expect("Ops!");
 
-    let (opcode, clock) = parse_instructions(input.as_str() );
-
     let sample_intervals = vec![20usize, 60, 100, 140, 180, 220];
     let mut sampling_interval = sample_intervals.iter().peekable();
 
     let mut crt = CRT::new(40);
     let mut cpu = CPU::new();
-    cpu.load(opcode);
 
+    let (opcode, clock) = parse_instructions(input.as_str() );
+    cpu.load(opcode);
 
     let sum = (1..=clock)
         .map(|cycle| {
