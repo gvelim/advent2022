55cd69cae51127c2cffcaf55f4661c8984676f92
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-01-09 20:00:14 +0000

    Day16 - Pass - Use of Backtrack algo to create the solution space - Sample dataset passes

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,5 +1,8 @@
+extern crate core;
+
 use std::cmp::Ordering;
 use std::collections::{HashMap, VecDeque};
+use std::fmt::Debug;
 use std::str::FromStr;
 
 const INPUT: &str = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
@@ -15,16 +18,53 @@ Valve JJ has flow rate=21; tunnel leads to valve II";
 
 
 fn main() {
-    let volcano = Volcano::parse(INPUT);
-    println!("Pressure: {}", greedy_search(&volcano,"AA") );
-    println!("Pressure: {}", path_pressure(&volcano, &["AA", "DD", "BB", "JJ", "HH", "EE", "CC"]) );
 
-    // create all path combinations starting from AA
+    struct Combinations<'a> {
+        path: Vec<&'a str>,
+        combos: Vec<Vec<&'a str>>
+    }
+    impl<'a> Combinations<'a> {
+        fn combinations(&mut self, valves: &[&'a str]) -> &Vec<&str> {
+
+            self.path.push(valves[0]);
+
+            if valves.len() == 1 {
+                // ok we got potential solution, store it
+                self.combos.push(self.path.clone());
+                self.path.pop();
+                return &self.path;
+            }
+
+            let mut tmp = valves[1..].to_vec();
+            (0..valves[1..].len())
+                .for_each(|_|{
+                    self.combinations(tmp.as_slice());
+                    tmp.rotate_right(1);
+                });
+
+            self.path.pop();
+            &self.path
+        }
+    }
+
+    let net = ValveNet::parse(INPUT);
+    let valves = ["AA", "DD", "BB", "JJ", "HH", "EE", "CC"];
+    println!("Pressure: {}", greedy_search(&net, "AA") );
+
+    // create all valve visit order combinations
+    let mut comb = Combinations{ path: vec![], combos: vec![] };
+    comb.combinations(&valves);
 
     // per path calculate total pressure released and select the max of all paths
+    let max = comb.combos.iter()
+        .map(|path|
+            (path_pressure(&net, path.as_slice()), path)
+        )
+        .max_by(|a,b| a.0.cmp(&b.0) );
+    println!("Solutions: {}\nMax flow {:?}",comb.combos.len(),max)
 }
 
-fn path_pressure(volcano:&Volcano, combinations: &[&str]) -> usize {
+fn path_pressure(volcano:&ValveNet, combinations: &[&str]) -> usize {
 
     let mut time_left = 30;
 
@@ -38,14 +78,14 @@ fn path_pressure(volcano:&Volcano, combinations: &[&str]) -> usize {
             } else {
                 time_left -= path.len(); // = len-1 steps + open valve
                 let total_pressure_released = volcano.flow[&target].pressure * time_left;
-                println!("====> Time {time_left}, {:?}", (&target, path.len(), total_pressure_released));
+                // println!("====> Time {time_left}, {:?}", (&target, path.len(), total_pressure_released));
                 Some(total_pressure_released)
             }
         })
         .sum::<usize>()
 }
 
-fn greedy_search(volcano:&Volcano, start: &str) -> usize {
+fn greedy_search(volcano:&ValveNet, start: &str) -> usize {
     let mut queue = VecDeque::new();
     // let mut combination = vec!["CC", "EE", "HH", "JJ", "BB", "DD", "AA"];
     let mut flow = volcano.flow.iter()
@@ -99,7 +139,7 @@ fn greedy_search(volcano:&Volcano, start: &str) -> usize {
 #[derive(Debug)]
 struct BFS();
 impl BFS {
-    fn find_path(vol:&Volcano, start:&String, end:&String) -> Vec<String> {
+    fn find_path(vol:&ValveNet, start:&String, end:&String) -> Vec<String> {
         let mut queue = VecDeque::new();
         let mut state: HashMap<String,(bool,Option<String>)> =
             vol.flow.iter()
@@ -137,13 +177,13 @@ struct Valve {
     open: bool
 }
 
-struct Volcano {
+struct ValveNet {
     graph: HashMap<String,Vec<String>>,
     flow: HashMap<String, Valve>
 }
 
-impl Volcano {
-    fn parse(input: &str) -> Volcano {
+impl ValveNet {
+    fn parse(input: &str) -> ValveNet {
         let (graph, flow) = input.lines()
             .map(|line| {
                 line.split(&[' ','=',';',','])
@@ -167,6 +207,6 @@ impl Volcano {
                 (g,f)
             });
 
-        Volcano { graph, flow }
+        ValveNet { graph, flow }
     }
 }
