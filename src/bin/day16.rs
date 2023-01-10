abaaa13541f15e905f793e92cb123d894a4c4445
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-01-10 14:17:46 +0000

    Day16 - Pass - refactor ValveNet

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -19,41 +19,8 @@ const BUDGET:usize = 30;
 
 fn main() {
 
-    struct ValveBacktrack<'a> {
-        path: Vec<&'a str>,
-        solution: Vec<&'a str>,
-        max: usize
-    }
-    impl<'a> ValveBacktrack<'a> {
-        fn combinations(&mut self, net: &ValveNet, valves: &[&'a str]) {
-
-            self.path.push(valves[0]);
-
-            if valves.len() == 1 {
-                // ok we got potential solution, store it
-                let pressure = net.path_pressure(BUDGET, &self.path);
-                if pressure > self.max {
-                    println!("Found {},{:?}",pressure,self.path);
-                    self.max = pressure;
-                    self.solution = self.path.clone();
-                }
-                self.path.pop();
-                return;
-            }
-
-            let mut tmp = valves[1..].to_vec();
-            (0..valves[1..].len())
-                .for_each(|i|{
-                    tmp.swap(0,i);
-                    self.combinations(net, tmp.as_slice());
-                });
-
-            self.path.pop();
-        }
-    }
-
     let input = std::fs::read_to_string("src/bin/day16_input.txt").expect("ops!");
-    let mut net = ValveNet::parse(input.as_str());
+    let mut net = ValveNet::parse(INPUT);
 
     let start = "AA";
     let valves = net.flow.iter()
@@ -64,64 +31,16 @@ fn main() {
         });
     println!("Valves: {:?}", valves );
     net.build_cache(&valves);
-    let (max,solution) = greedy_search(&net, BUDGET, start);
+    let (max,solution) = net.greedy_search(BUDGET, start);
     println!("Pressure (Greedy): {}\nPath: {:?}", max, solution );
 
     // create all valve visit order combinations
-    let mut comb = ValveBacktrack { path: vec![], solution: vec![], max };
-    comb.combinations(&net,&solution);
+    let mut comb = net.backtrack(max);
+    comb.combinations(&solution);
 
     println!("Solutions: {:?}\nMax flow {:?}", comb.solution, comb.max)
 }
 
-fn greedy_search<'a>(net:&'a ValveNet, mut time_left:usize, start: &'a str) -> (usize,Vec<&'a str>) {
-    let mut queue = VecDeque::new();
-    // let mut combination = vec!["CC", "EE", "HH", "JJ", "BB", "DD", "AA"];
-    let mut flow = net.flow.iter()
-        .map(|(key,valve)| (key, valve.clone()))
-        .collect::<HashMap<_,_>>();
-    let mut path = vec![start];
-
-    queue.push_back(start);
-
-    let mut pressure = 0;
-
-    while let Some(valve) = queue.pop_front() {
-
-        flow.get_mut(&valve).unwrap().open = true;
-
-        let mut options = flow.iter()
-            .filter(|&(_,valve)| valve.pressure > 0  && !valve.open )
-            .map(|(&target,_)|
-                (target, net.find_path_cost(&valve, &target).unwrap())
-            )
-            .map(|(&target,cost)|
-                (target, cost, net.flow[target].pressure/cost)
-            )
-            .collect::<Vec<_>>();
-
-        options.sort_by(|a,b|
-            match a.2.cmp(&b.2) {
-                res@
-                (Ordering::Less | Ordering::Greater) => res,
-                Ordering::Equal => b.1.cmp(&a.1)
-            }
-        );
-
-        if let Some((valve,cost,value)) = options.pop() {
-            path.push(valve);
-            if time_left < cost {
-                path.extend(options.iter().map(|&(v,..)| v).rev());
-                return (pressure,path)
-            }
-            time_left -= cost;
-            pressure += net.flow[&valve].pressure * time_left;
-            println!("====> Time: {time_left} got for Option {:?} out of Options: {:?}", (&valve, cost, value, time_left, pressure), options);
-            queue.push_back(valve);
-        }
-    }
-    (pressure,path)
-}
 
 struct Cache<'a> {
     cache: Cell<HashMap<(&'a str,&'a str),usize>>
@@ -157,6 +76,40 @@ impl<'a> Cache<'a> {
     }
 }
 
+struct ValveBacktrack<'a> {
+    net: &'a ValveNet<'a>,
+    path: Vec<&'a str>,
+    solution: Vec<&'a str>,
+    max: usize
+}
+impl<'a> ValveBacktrack<'a> {
+    fn combinations(&mut self, valves: &[&'a str]) {
+
+        self.path.push(valves[0]);
+
+        if valves.len() == 1 {
+            // ok we got potential solution, store it
+            let pressure = self.net.path_pressure(BUDGET, &self.path);
+            if pressure > self.max {
+                println!("Found {},{:?}",pressure,self.path);
+                self.max = pressure;
+                self.solution = self.path.clone();
+            }
+            self.path.pop();
+            return;
+        }
+
+        let mut tmp = valves[1..].to_vec();
+        (0..valves[1..].len())
+            .for_each(|i|{
+                tmp.swap(0,i);
+                self.combinations(tmp.as_slice());
+            });
+
+        self.path.pop();
+    }
+}
+
 #[derive(Debug, Copy, Clone)]
 struct Valve {
     pressure: usize,
@@ -170,6 +123,9 @@ struct ValveNet<'a> {
 }
 
 impl<'a> ValveNet<'a> {
+    fn backtrack(&'a self, max: usize) -> ValveBacktrack<'a> {
+        ValveBacktrack { net: self, path: vec![], solution: vec![], max }
+    }
     fn build_cache(&mut self, valves: &[&'a str]) {
         let mut cache = Cache { cache: Cell::new(HashMap::new()) };
         cache.build(self, valves);
@@ -230,6 +186,55 @@ impl<'a> ValveNet<'a> {
         }
         None
     }
+    fn greedy_search(&'a self, mut time_left:usize, start: &'a str) -> (usize,Vec<&'a str>) {
+        let mut queue = VecDeque::new();
+        // let mut combination = vec!["CC", "EE", "HH", "JJ", "BB", "DD", "AA"];
+        let mut flow = self.flow.iter()
+            .map(|(key,valve)| (key, valve.clone()))
+            .collect::<HashMap<_,_>>();
+        let mut path = vec![start];
+
+        queue.push_back(start);
+
+        let mut pressure = 0;
+
+        while let Some(valve) = queue.pop_front() {
+
+            flow.get_mut(&valve).unwrap().open = true;
+
+            let mut options = flow.iter()
+                .filter(|&(_,valve)| valve.pressure > 0  && !valve.open )
+                .map(|(&target,_)|
+                    (target, self.find_path_cost(&valve, &target).unwrap())
+                )
+                .map(|(&target,cost)|
+                    (target, cost, self.flow[target].pressure/cost)
+                )
+                .collect::<Vec<_>>();
+
+            options.sort_by(|a,b|
+                match a.2.cmp(&b.2) {
+                    res@
+                    (Ordering::Less | Ordering::Greater) => res,
+                    Ordering::Equal => b.1.cmp(&a.1)
+                }
+            );
+
+            if let Some((valve,cost,value)) = options.pop() {
+                path.push(valve);
+                if time_left < cost {
+                    path.extend(options.iter().map(|&(v,..)| v).rev());
+                    return (pressure,path)
+                }
+                time_left -= cost;
+                pressure += self.flow[&valve].pressure * time_left;
+                println!("====> Time: {time_left} got for Option {:?} out of Options: {:?}", (&valve, cost, value, time_left, pressure), options);
+                queue.push_back(valve);
+            }
+        }
+        (pressure,path)
+    }
+
     fn parse(input: &str) -> ValveNet {
         let (graph, flow) = input.lines()
             .map(|line| {
