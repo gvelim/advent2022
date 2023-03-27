1bd5aebeaaac0f69f820f59a60c81973a3220efe
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-03-27 21:54:58 +0100

    langton's ant - better autoscaling - removed Ant flickering

--- src/bin/langton_ant.rs
+++ src/bin/langton_ant.rs
@@ -8,9 +8,9 @@ use bracket_lib::prelude::*;
 fn main() -> BResult<()> {
     let board = Board::init();
 
-    let ctx = BTermBuilder::simple80x50()
-        .with_automatic_console_resize(true)
-        .with_fps_cap(120f32)
+    let ctx = BTermBuilder::simple(160, 100)?
+        .with_sparse_console(160, 100, "terminal8x8.png")
+        .with_fps_cap(10f32)
         .with_title("Langton's Ant - Press 'Q' to exit")
         .build()?;
 
@@ -99,38 +99,47 @@ impl Board {
             }
         }
     }
-    fn tick(&mut self) {
-        let p = self.ant.pos;
-
+    fn capture_point(&mut self, p: (isize,isize)) {
         self.border.0 = min(p.0,self.border.0);
         self.border.1 = min(p.1,self.border.1);
         self.border.2 = max(p.0,self.border.2);
         self.border.3 = max(p.1,self.border.3);
         self.area.0 = self.border.2 - self.border.0 + 1;
         self.area.1 = self.border.3 - self.border.1 + 1;
-
+    }
+    fn tick(&mut self) {
+        let p = self.ant.pos;
+        self.capture_point(p);
         let sqr = self.square_colour(p);
         self.ant.tick(sqr);
         self.inverse_square(p);
     }
     fn draw(&self, ctx:&mut BTerm) {
+        ctx.set_active_console(0);
         ctx.set_scale(
-            f32::min(80f32.div((self.area.0) as f32),50f32.div((self.area.1) as f32)),
-            80,  50
+            f32::min(160f32.div((self.area.0+3) as f32),100f32.div((self.area.1+3) as f32)),
+            (80 + (self.border.0 + self.border.2)) as i32, (50 + (self.border.1 + self.border.3)) as i32
         );
         for y in self.border.1-1 ..= self.border.3+1 {
             for x in self.border.0-1 ..= self.border.2+1 {
                 let (colour, char) =
-                    match self.map.get(&(x,y)) {
-                        Some(Square::Black) => (BLACK,'B'),
+                if x == self.ant.pos.0 && y == self.ant.pos.1 {
+                    (RED, '@')
+                } else {
+                    match self.map.get(&(x, y)) {
+                        Some(Square::Black) => (BLACK, 'B'),
                         Some(Square::White) => (WHITE, 'W'),
-                        None => ( GREEN, '.' )
-                    };
+                        None => (GREEN, '.')
+                    }
+                };
                 ctx.set(x+80, y+50, colour, BLUE, to_cp437(char) );
             }
         }
-        let (x,y) = self.ant.pos;
-        ctx.set( x+80, y+50, RED, BLUE, to_cp437('@'));
+        // let (x,y) = self.ant.pos;
+        // ctx.set( x+80, y+50, RED, BLUE, to_cp437('@'));
+        ctx.set_active_console(1);
+        ctx.print(0,0,format!("Border: {:?}",self.border));
+        ctx.print(0,2,format!("Area: {:?}",self.area));
     }
 }
 
