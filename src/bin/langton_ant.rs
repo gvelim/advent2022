d274c211127bf99c837629c2a97b605bf7a6fd44
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-04-06 20:06:19 +0100

    Langton - refactor - Move draw() & tick() concerns onto LangtonAnt Struct - Keep Board,Ant struct agnostic

--- src/bin/langton_ant.rs
+++ src/bin/langton_ant.rs
@@ -6,7 +6,6 @@ use std::vec;
 use bracket_lib::prelude::*;
 
 fn main() -> BResult<()> {
-    let board = Board::init();
 
     let ctx = BTermBuilder::simple(160, 100)?
         .with_simple_console(640,480,"terminal8x8.png")
@@ -16,20 +15,83 @@ fn main() -> BResult<()> {
         .with_title("Langton's Ant - Press 'Q':exit, 'A':Ant, 'I':Invert")
         .build()?;
 
-    main_loop(ctx, board)
+    let lant = LangtonsAnt {
+        board: Board::init(),
+        ants: vec![Ant::init((0, 0))]
+    };
+
+    main_loop(ctx, lant)
 }
 
-impl GameState for Board {
+struct LangtonsAnt {
+    board: Board,
+    ants: Vec<Ant>
+}
+
+impl LangtonsAnt {
+    fn inject_ant(&mut self) { self.ants.push(Ant::init((0, 0))) }
+    fn draw(&self, ctx:&mut BTerm) {
+        let LangtonsAnt{board, ants: ant } = self;
+        ctx.set_active_console(1);
+        ctx.cls_bg(BLACK);
+        let (off_x, off_y) = (320 - board.border.2 - board.border.0, 240 - board.border.3 - board.border.1);
+        ctx.set_scale(
+            f32::min(640f32.div((board.area.0+10) as f32),480f32.div((board.area.1+10) as f32)),
+            off_x, off_y
+        );
+        for y in board.border.1-1 ..= board.border.3+1 {
+            for x in board.border.0-1 ..= board.border.2+1 {
+                ctx.set_bg(x + off_x, y + off_y,
+                           board.map
+                               .get(&(x,y))
+                               .map(|&sqr| match sqr { Square::Black => BLACK, _ => WHITE } )
+                               .unwrap_or(BLUE)
+                )
+            }
+        }
+        ant.iter()
+            .for_each(|ant|
+                ctx.set_bg( ant.pos.0+off_x, ant.pos.1+off_y, RED )
+            );
+    }
+    fn draw_stats(&self, ctx:&mut BTerm) {
+        let LangtonsAnt{board, ants: ant } = self;
+        ctx.set_active_console(2);
+        ctx.cls_bg(BLACK);
+        ctx.print(0,0,format!("Corners: {:?}",board.border));
+        ctx.print(0,2,format!("Offset: {:?}",(
+            (board.border.0 + board.border.2, board.border.1 + board.border.3),
+            (320 - board.border.2 - board.border.0, 240 - board.border.3 - board.border.1)
+        )
+        ));
+        ctx.print(0,4,format!("Area: {:?}",board.area));
+        ctx.print(0,6,format!("Population: {:?}",ant.len()));
+        ctx.print(0,47,format!("FPS: {:?}",ctx.fps));
+        ctx.print(0,49,format!("Render time: {:?}(ms)",ctx.frame_time_ms));
+    }
+}
+
+impl GameState for LangtonsAnt {
     fn tick(&mut self, ctx: &mut BTerm) {
+        let LangtonsAnt{ board, ants } = self;
+
+        ants.iter_mut()
+            .all(|ant| {
+                let p = ant.pos;
+                ant.tick(*board.square_colour(p));
+                board.tick(p);
+                true
+            });
+
+        self.draw(ctx);
+        self.draw_stats(ctx);
+
         match ctx.key {
             Some(VirtualKeyCode::Q) => ctx.quit(),
             Some(VirtualKeyCode::A) => self.inject_ant(),
-            Some(VirtualKeyCode::I) => self.invert_board(),
+            Some(VirtualKeyCode::I) => self.board.invert_board(),
             _ => {}
         }
-        self.tick();
-        self.draw(ctx);
-        self.draw_stats(ctx);
     }
 }
 
@@ -100,8 +162,7 @@ impl Ant {
 struct Board {
     border: (i32,i32,i32,i32),
     area: (i32,i32),
-    map: HashMap<(i32,i32),Square>,
-    ant: Vec<Ant>
+    map: HashMap<(i32,i32),Square>
 }
 impl Board {
     fn init() -> Board {
@@ -109,11 +170,7 @@ impl Board {
             border:(-1,-1,1,1),
             area: (3,3),
             map:HashMap::new(),
-            ant: vec![Ant::init((0, 0))]
         }
-    }
-    fn inject_ant(&mut self) {
-        self.ant.push(Ant::init((0, 0)))
     }
     fn square_colour(&mut self, p: (i32, i32)) -> &mut Square {
         self.map.entry(p).or_insert(Square::default())
@@ -129,51 +186,9 @@ impl Board {
         self.area.0 = self.border.2 - self.border.0 + 1;
         self.area.1 = self.border.3 - self.border.1 + 1;
     }
-    fn tick(&mut self) {
-        (0..self.ant.len()).for_each(|i|{
-            let p = self.ant[i].pos;
-            self.capture_point(p);
-            let sqr = *self.square_colour(p);
-            self.ant[i].tick(sqr);
-            self.square_colour(p).inverse();
-        })
-    }
-    fn draw(&self, ctx:&mut BTerm) {
-        ctx.set_active_console(1);
-        ctx.cls_bg(BLACK);
-        let (off_x, off_y) = (320 - self.border.2 - self.border.0, 240 - self.border.3 - self.border.1);
-        ctx.set_scale(
-            f32::min(640f32.div((self.area.0+10) as f32),480f32.div((self.area.1+10) as f32)),
-            off_x, off_y
-        );
-        for y in self.border.1-1 ..= self.border.3+1 {
-            for x in self.border.0-1 ..= self.border.2+1 {
-                ctx.set_bg(x + off_x, y + off_y,
-                           self.map
-                               .get(&(x,y))
-                               .map(|&sqr| match sqr { Square::Black => BLACK, _ => WHITE } )
-                               .unwrap_or(BLUE)
-                )
-            }
-        }
-        self.ant.iter()
-            .for_each(|ant|
-                ctx.set_bg( ant.pos.0+off_x, ant.pos.1+off_y, RED )
-            );
-    }
-    fn draw_stats(&self, ctx:&mut BTerm) {
-        ctx.set_active_console(2);
-        ctx.cls_bg(BLACK);
-        ctx.print(0,0,format!("Corners: {:?}",self.border));
-        ctx.print(0,2,format!("Offset: {:?}",(
-            (self.border.0 + self.border.2, self.border.1 + self.border.3),
-            (320 - self.border.2 - self.border.0, 240 - self.border.3 - self.border.1)
-        )
-        ));
-        ctx.print(0,4,format!("Area: {:?}",self.area));
-        ctx.print(0,6,format!("Population: {:?}",self.ant.len()));
-        ctx.print(0,47,format!("FPS: {:?}",ctx.fps));
-        ctx.print(0,49,format!("Render time: {:?}(ms)",ctx.frame_time_ms));
+    fn tick(&mut self, p: (i32,i32)) {
+        self.capture_point(p);
+        self.square_colour(p).inverse();
     }
 }
 
@@ -183,10 +198,10 @@ impl Display for Board {
         for y in self.border.3-1 ..= self.border.1+1 {
             for x in self.border.0-1 ..= self.border.2+1 {
                 write!(f,"{:3}",
-                       match self.map.get(&(x,y)) {
-                           None => '.',
-                           Some(&sqr) => if sqr == Square::Black { 'B' } else { 'w' }
-                       }
+                       self.map
+                           .get(&(x,y))
+                           .map(|&sqr| match sqr { Square::Black =>'B', _ => 'w' })
+                           .unwrap_or('.')
                 )?
             }
             writeln!(f)?
