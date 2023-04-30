ad077becbb32bd5cf6a12cd5cd05cdb5778d5f39
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-04-30 15:29:28 +0100

    Day12 - Moving Grid<T> onto library

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,45 @@ impl From<(usize,usize)> for Coord {
     }
 }
 
+pub struct Grid<T> {
+    pub width: usize,
+    pub height: usize,
+    pub grid: Vec<T>,
+}
+impl<T> Grid<T>
+    where T : Default + Copy {
+    pub fn new(width: usize, height: usize) -> Grid<T> {
+        Grid {
+            height,
+            width,
+            grid: vec![T::default(); width * height],
+        }
+    }
+    pub fn in_bounds(&self, p:Coord) -> bool {
+        p.x < self.width && p.y < self.height
+    }
+    pub fn square(&self, p: Coord) -> Option<&T> {
+        if !self.in_bounds(p) {
+            return None
+        }
+        Some(&self.grid[p.y * self.width + p.x])
+    }
+    pub fn square_mut(&mut self, p: Coord) -> Option<&mut T> {
+        if !self.in_bounds(p) {
+            return None
+        }
+        Some(&mut self.grid[p.y * self.width + p.x])
+    }
+    pub fn neighbouring(&self, cs:Coord) -> impl Iterator<Item=(Coord,&'_ T)> {
+        let delta = [(0, -1), (1, 0), (-1, 0), (0, 1)];
+        delta.into_iter()
+            .filter_map(move |d|{
+                let ns = Coord {
+                    x: cs.x.saturating_add_signed(d.0),
+                    y: cs.y.saturating_add_signed(d.1)
+                };
+                self.square(ns)
+                    .map(|val| (ns,val) )
+            })
+    }
+}
