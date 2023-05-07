ceb0bed85fcf5f44dbd69a61cb76501d7d541ef4
george velimachitis <3185416+gvelim@users.noreply.github.com>
2023-05-07 19:12:23 +0100

    Adding Tag(board)

--- src/bin/langton_ant_ecs.rs
+++ src/bin/langton_ant_ecs.rs
@@ -3,7 +3,6 @@ use std::hash::Hash;
 use bracket_lib::color::{BLUE, RED, WHITE};
 use bracket_lib::prelude::{BResult, BTerm, BTermBuilder, GameState, main_loop};
 use specs::prelude::*;
-use specs::shred::Fetch;
 use specs_derive::*;
 use crate::Square::{Black, White};
 
@@ -17,6 +16,7 @@ fn main() -> BResult<()> {
     sim.world.register::<Direction>();
     sim.world.register::<AntStepMove>();
     sim.world.register::<Ant>();
+    sim.world.register::<Board>();
 
     sim.world.create_entity()
         .with(Coord(0,0))
@@ -28,10 +28,11 @@ fn main() -> BResult<()> {
     sim.world.create_entity()
         .with(Coord(0,0))
         .with(Square::default())
+        .with(Board)
         .build();
 
     let ctx = BTermBuilder::simple80x50()
-        .with_fps_cap(5f32)
+        .with_fps_cap(15f32)
         .with_title("Langton Ant - ECS")
         .build()?;
 
@@ -51,10 +52,11 @@ impl GameState for Simulation {
         let pos = self.world.read_storage::<Coord>();
         let square = self.world.read_storage::<Square>();
         let ant = self.world.read_storage::<Ant>();
+        let board = self.world.read_storage::<Board>();
 
-        (&pos, &square).join()
+        (&board, &pos, &square).join()
             // .inspect(|d| println!("Draw: {:?}",d))
-            .for_each(|(p, s)|
+            .for_each(|(.., p, s)|
                 ctx.set_bg(p.0 + CENTER.0, p.1 + CENTER.1, match s {
                     Black => BLUE,
                     White => WHITE
@@ -116,30 +118,30 @@ impl<'a> System<'a> for AntStepMove {
     type SystemData = (
         Entities<'a>,
         WriteStorage<'a, Direction>, WriteStorage<'a, Coord>,
-        WriteStorage<'a, Square>
+        WriteStorage<'a, Square>, WriteStorage<'a, Board>
     );
 
     fn run(&mut self, data: Self::SystemData) {
         let (
             ent,
             mut dir, mut apos,
-            mut sqr
+            mut sqr, mut board
         ) = data;
-        let mut new_square = (false,None);
 
+        let mut new_square = (false,None);
         let mut squares = (&apos, &mut sqr).join().map(|d| (*d.0, d.1)).collect::<HashMap<Coord, _>>();
 
         (&ent, &mut dir, &mut apos).join()
-            .inspect(|p| println!("{:?}",&p))
+            // .inspect(|p| println!("{:?}",&p))
             .for_each(|(.., d, p)| {
 
-                let mut def = Square::default();
+                let mut default = Square::default();
 
                 let sqr =
                     if let Some(sqr) = squares.get_mut(p) { sqr }
                     else {
                         new_square = (true, Some(*p));
-                        &mut def
+                        &mut default
                     };
 
                match match sqr {
@@ -162,6 +164,7 @@ impl<'a> System<'a> for AntStepMove {
                 ent.build_entity()
                     .with(pos, &mut apos)
                     .with(Black, &mut sqr)
+                    .with(Board, &mut board)
                     .build();
             },
             _ => {}
